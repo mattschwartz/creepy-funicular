55da37f6b7bee46cffc513bdf6ccd7f0f1fab337
Andrew Markley <ajm501028@gmail.com>
2015-09-26 15:54:30 -0500

    working on getting deep dream installed

--- creepyfun/src/deepdream.js
+++ creepyfun/src/deepdream.js
@@ -1,5 +1,10 @@
+var PythonShell = require('python-shell');
+
 module.exports = {
 	dream: function (pattern, img) {
-		console.log('dreaming...');
+		PythonShell.run('my_script.py', function (err) {
+			if (err) throw err;
+			console.log('finished');
+		});
 	}
-} 
+}
