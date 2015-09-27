8c03d579afa3df1ded0740f8fb1108adb2cef097
Matt Schwartz <schwamat@gmail.com>
2015-09-26 20:19:45 -0500

    .

--- creepyfun/src/deepdream.js
+++ creepyfun/src/deepdream.js
@@ -2,10 +2,12 @@ var PythonShell = require('python-shell');
 
 module.exports = {
 	dream: function (pattern, img) {
-		var filename = '/output/' + 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+		var filename = '/../assets/images/uploads/' + 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
 			var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
 			return v.toString(16);
-		}); + '.jpg';
+		}) + '.jpg';
+        
+        return filename;
 
 		var process = '/dream/dreamify.py -o ' + img + ' -g ' + pattern + ' -s ' + filename;
 
