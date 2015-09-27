53bd6922eb8f995a6ea0ec027ed51762cdb88bf1
Matt Schwartz <schwamat@gmail.com>
2015-09-26 21:56:51 -0500

    stylin

--- creepyfun/assets/js/_dreams.js
+++ creepyfun/assets/js/_dreams.js
@@ -3,4 +3,14 @@ $(document).ready(function () {
         $('#CopyDirectLink').select();
         document.execCommand("copy");
     })
+    
+    $('#DislikeButton').on('click', function() {
+        $(this).toggleClass('btn-primary')
+        $(this).toggleClass('btn-danger')
+    })
+    
+    $('#LikeButton').on('click', function() {
+        $(this).toggleClass('btn-primary')
+        $(this).toggleClass('btn-success')
+    })
 })
